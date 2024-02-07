6b369e9928429caa4e20c62f4f105d45ccb0fddc
Eric Cheng <online.eric@gmail.com>
2024-02-07 23:02:00 +1300

    type fixing format-time.ts

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -1,24 +1,22 @@
 import { format, getTime, formatDistanceToNow } from 'date-fns';
 
-// ----------------------------------------------------------------------
-
-export function fDate(date, newFormat) {
+export function fDate(date: Date | string | number | null, newFormat?: string): string {
   const fm = newFormat || 'dd MMM yyyy';
 
   return date ? format(new Date(date), fm) : '';
 }
 
-export function fDateTime(date, newFormat) {
+export function fDateTime(date: Date | string | number | null, newFormat?: string): string {
   const fm = newFormat || 'dd MMM yyyy p';
 
   return date ? format(new Date(date), fm) : '';
 }
 
-export function fTimestamp(date) {
+export function fTimestamp(date: Date | string | number | null): number | string {
   return date ? getTime(new Date(date)) : '';
 }
 
-export function fToNow(date) {
+export function fToNow(date: Date | string | number | null): string {
   return date
     ? formatDistanceToNow(new Date(date), {
         addSuffix: true,
