11ec43b62851cf01a3afe86796655c02f3d95de6
Eric Cheng <online.eric@gmail.com>
2024-02-10 18:05:18 +1300

    types fixing

--- src/components/iconify/iconify.tsx
+++ src/components/iconify/iconify.tsx
@@ -1,26 +1,24 @@
-import PropTypes from 'prop-types';
-import { forwardRef } from 'react';
-import { Icon } from '@iconify/react';
+import { Icon, InlineIcon } from '@iconify/react';
+import { IconifyIcon } from '@iconify/types';
+import { Box, SxProps, Theme } from '@mui/material';
 
-import Box from '@mui/material/Box';
+interface IconifyProps {
+	icon: IconifyIcon;
+	width?: string | number;
+	height?: string | number;
+	color?: string;
+	inline?: boolean;
+	sx?: SxProps<Theme>;
+}
 
-// ----------------------------------------------------------------------
-
-const Iconify = forwardRef(({ icon, width = 20, sx, ...other }, ref) => (
-  <Box
-    ref={ref}
-    component={Icon}
-    className="component-iconify"
-    icon={icon}
-    sx={{ width, height: width, ...sx }}
-    {...other}
-  />
-));
-
-Iconify.propTypes = {
-  icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
-  sx: PropTypes.object,
-  width: PropTypes.number,
-};
+const Iconify: React.FC<IconifyProps> = ({ icon, width = 20, height = 20, color = 'currentColor', inline = false, sx }) => inline ? (
+		<Box className="component-iconify" sx={{ width, height, ...sx }}>
+			<InlineIcon icon={icon} width={width} height={height} color={color} />
+		</Box>
+	) : (
+		<Box className="component-iconify" sx={{ width, height, ...sx }}>
+			<Icon icon={icon} width={width} height={height} color={color} />
+		</Box>
+	);
 
 export default Iconify;
